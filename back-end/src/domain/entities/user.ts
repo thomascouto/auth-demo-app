0dac476323ccef53c0e28bca70dfda79a1e45611
Thomas Couto (DESKTOP) <thomascouto@gmail.com>
2022-03-24 10:20:37 -0300

    feat: new field

--- back-end/src/domain/entities/user.ts
+++ back-end/src/domain/entities/user.ts
@@ -4,19 +4,24 @@ import { AbstractBaseEntity } from './abstractBaseEntity'
 type UserProps = {
 	username: string
 	password: string
+	isAdmin: boolean
 }
 
 @Entity()
 export class User extends AbstractBaseEntity {
-	@Property({ nullable: false })
+	@Property({ nullable: false, unique: true })
 	username!: string
 
 	@Property({ nullable: false })
 	password!: string
 
-	set props({ username, password }: UserProps) {
+	@Property({ default: false })
+	isAdmin!: boolean
+
+	set props({ username, password, isAdmin }: UserProps) {
 		this.username = username
 		this.password = password
+		this.isAdmin = isAdmin
 	}
 
 	constructor(props?: UserProps) {
