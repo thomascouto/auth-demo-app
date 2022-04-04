1716ce90017a4430e5a43c2989cc1a6af585357e
Thomas Couto (DESKTOP) <thomascouto@gmail.com>
2022-04-04 15:47:30 -0300

    chore: setup dependencies

---
+++ back-end-server/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+	env: {
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		tsconfigRootDir: __dirname,
+	},
+	plugins: ['@typescript-eslint', 'prettier'],
+	rules: {
+		'prettier/prettier': ['error'],
+	},
+}
