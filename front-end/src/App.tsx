efb6d1754e825bac451ff9f936be361adaa0d61a
Thomas Couto (DESKTOP) <thomascouto@gmail.com>
2022-04-14 12:26:38 -0300

    chore: add react-cookie dep

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { CookiesProvider } from "react-cookie"
 import { Routes, Route } from "react-router"
 import NotFound from "./components/404"
 import Admin from "./components/Admin"
@@ -13,22 +14,24 @@ const App = () => {
 	const globalState = useGlobalState()
 
 	return (
-		<GlobalStateContext.Provider value={globalState}>
-			<Routes>
-				<Route path="/" element={<Layout />}>
-					<Route path="login" element={<Login />} />
+		<CookiesProvider>
+			<GlobalStateContext.Provider value={globalState}>
+				<Routes>
+					<Route path="/" element={<Layout />}>
+						<Route path="login" element={<Login />} />
 
-					<Route element={<PrivateRoute adminRule={true} />}>
-						<Route path="admin" element={<Admin />} />
-					</Route>
+						<Route element={<PrivateRoute adminRule={true} />}>
+							<Route path="admin" element={<Admin />} />
+						</Route>
 
-					<Route element={<PrivateRoute adminRule={false} />}>
-						<Route path="/" element={<Main />} />
+						<Route element={<PrivateRoute adminRule={false} />}>
+							<Route path="/" element={<Main />} />
+						</Route>
 					</Route>
-				</Route>
-				<Route path="*" element={<NotFound />} />
-			</Routes>
-		</GlobalStateContext.Provider>
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</GlobalStateContext.Provider>
+		</CookiesProvider>
 	)
 }
 
